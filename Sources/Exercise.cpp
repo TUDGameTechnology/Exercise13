a7092d1a80eebb781f2a99cb01a84900cc84feed
Robert Konrad <robert@ktx-software.com>
2014-11-21 17:32:45 +0100

    Added level mesh. Use matrices.

--- Sources/Exercise.cpp
+++ Sources/Exercise.cpp
@@ -14,6 +14,52 @@
 
 using namespace Kore;
 
+class MeshObject {
+public:
+	MeshObject(const char* meshFile, const char* textureFile, const VertexStructure& structure, float scale = 1.0f) {
+		mesh = loadObj(meshFile);
+		image = new Texture(textureFile, true);
+
+		vertexBuffer = new VertexBuffer(mesh->numVertices, structure);
+		float* vertices = vertexBuffer->lock();
+		for (int i = 0; i < mesh->numVertices; ++i) {
+			vertices[i * 8 + 0] = mesh->vertices[i * 8 + 0] * scale;
+			vertices[i * 8 + 1] = mesh->vertices[i * 8 + 1] * scale;
+			vertices[i * 8 + 2] = mesh->vertices[i * 8 + 2] * scale;
+			vertices[i * 8 + 3] = mesh->vertices[i * 8 + 3];
+			vertices[i * 8 + 4] = 1.0f - mesh->vertices[i * 8 + 4];
+			vertices[i * 8 + 5] = mesh->vertices[i * 8 + 5];
+			vertices[i * 8 + 6] = mesh->vertices[i * 8 + 6];
+			vertices[i * 8 + 7] = mesh->vertices[i * 8 + 7];
+		}
+
+		vertexBuffer->unlock();
+
+		indexBuffer = new IndexBuffer(mesh->numFaces * 3);
+		int* indices = indexBuffer->lock();
+		for (int i = 0; i < mesh->numFaces * 3; i++) {
+			indices[i] = mesh->indices[i];
+		}
+		indexBuffer->unlock();
+
+		M = mat4::Identity();
+	}
+
+	void render(TextureUnit tex) {
+		image->set(tex);
+		vertexBuffer->set();
+		indexBuffer->set();
+		Graphics::drawIndexedVertices();
+	}
+
+	mat4 M;
+private:
+	VertexBuffer* vertexBuffer;
+	IndexBuffer* indexBuffer;
+	Mesh* mesh;
+	Texture* image;
+};
+
 namespace {
 	const int width = 1024;
 	const int height = 768;
@@ -21,24 +67,30 @@ namespace {
 	Shader* vertexShader;
 	Shader* fragmentShader;
 	Program* program;
-	VertexBuffer* vertexBuffer;
-	IndexBuffer* indexBuffer;
-	Mesh* mesh;
-	Texture* image;
+	MeshObject* objects[] = { nullptr, nullptr, nullptr, nullptr, nullptr };
 	TextureUnit tex;
+	ConstantLocation pvLocation;
+	ConstantLocation mLocation;
+	mat4 PV;
 
 	void update() {
 		float t = (float)(System::time() - startTime);
 		Kore::Audio::update();
 		
 		Graphics::begin();
-		Graphics::clear(Graphics::ClearColorFlag | Graphics::ClearDepthFlag, 0xff000000);
+		Graphics::clear(Graphics::ClearColorFlag | Graphics::ClearDepthFlag, 0xff9999FF, 1000.0f);
 		
 		program->set();
-		image->set(tex);
-		vertexBuffer->set();
-		indexBuffer->set();
-		Graphics::drawIndexedVertices();
+
+		PV = mat4::Perspective(60, (float)width / (float)height, 0.1f, 100) * mat4::lookAt(vec3(0, 2, -3), vec3(0, 2, 0), vec3(0, 1, 0));
+		Graphics::setMatrix(pvLocation, PV);
+
+		MeshObject** current = &objects[0];
+		while (*current != nullptr) {
+			Graphics::setMatrix(mLocation, (*current)->M);
+			(*current)->render(tex);
+			++current;
+		}
 
 		Graphics::end();
 		Graphics::swapBuffers();
@@ -69,8 +121,7 @@ namespace {
 	}
 
 	void init() {
-		mesh = loadObj("Level/ball.obj");
-		image = new Texture("Level/ball_texture.jpg", true);
+		
 
 		//mesh = loadObj("Level/level.obj");
 		//image = new Texture("Level/texture.jpg", true);
@@ -92,31 +143,14 @@ namespace {
 		program->link(structure);
 
 		tex = program->getTextureUnit("tex");
+		pvLocation = program->getConstantLocation("PV");
+		mLocation = program->getConstantLocation("M");
 
-		// Set this to 1.0f when you do your transformations in the vertex shader
-		float scale = 0.5f;
-
-		vertexBuffer = new VertexBuffer(mesh->numVertices, structure);
-		float* vertices = vertexBuffer->lock();
-		for (int i = 0; i < mesh->numVertices; ++i) {
-			vertices[i * 8 + 0] = mesh->vertices[i * 8 + 0] * scale;
-			vertices[i * 8 + 1] = mesh->vertices[i * 8 + 1] * scale;
-			vertices[i * 8 + 2] = mesh->vertices[i * 8 + 2] * scale;
-			vertices[i * 8 + 3] = mesh->vertices[i * 8 + 3];
-			vertices[i * 8 + 4] = 1.0f - mesh->vertices[i * 8 + 4];
-			vertices[i * 8 + 5] = mesh->vertices[i * 8 + 5];
-			vertices[i * 8 + 6] = mesh->vertices[i * 8 + 6];
-			vertices[i * 8 + 7] = mesh->vertices[i * 8 + 7];
-		}
-
-		vertexBuffer->unlock();
-
-		indexBuffer = new IndexBuffer(mesh->numFaces * 3);
-		int* indices = indexBuffer->lock();
-		for (int i = 0; i < mesh->numFaces * 3; i++) {
-			indices[i] = mesh->indices[i];
-		}
-		indexBuffer->unlock();
+		objects[0] = new MeshObject("Level/ball.obj", "Level/unshaded.png", structure);
+		objects[0]->M = mat4::Translation(0.7f, 1.2f, -0.2f);
+		objects[1] = new MeshObject("Level/Level_top.obj", "Level/basicTiles6x6.png", structure);
+		objects[2] = new MeshObject("Level/Level_yellow.obj", "Level/basicTiles3x3yellow.png", structure);
+		objects[3] = new MeshObject("Level/Level_red.obj", "Level/basicTiles3x3red.png", structure);
 
 		Graphics::setRenderState(DepthTest, true);
 		Graphics::setRenderState(DepthTestCompare, ZCompareLess);
